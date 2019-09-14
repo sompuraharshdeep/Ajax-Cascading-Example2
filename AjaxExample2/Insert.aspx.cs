c774a4bd59422504cbe7dc8ea274af3e44578407
Harshdeep Sompura <sompuraharshdeep@gmail.com>
2019-09-14 18:35:43 +0530

    Add project files.

---
+++ AjaxExample2/Insert.aspx.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+
+namespace AjaxExample2
+{
+    public partial class Insert : System.Web.UI.Page
+    {
+
+        SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["connStr"].ConnectionString);
+        string opr, empName;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            opr = Request.QueryString["opr"].ToString();
+            if (opr == "search")
+            {
+                empName = Request.QueryString["name"].ToString();
+                con.Open();
+                SqlCommand cmd = new SqlCommand("select * from employee where empId like('" + empName.ToString() + "%')", con);
+                cmd.ExecuteNonQuery();
+                DataTable dt = new DataTable();
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                da.Fill(dt);
+                Response.Write("<table style='boader= bold 1'>");
+                Response.Write("<tr>");
+                Response.Write("<td>"); Response.Write("Name"); Response.Write("</td>");
+                Response.Write("<td>"); Response.Write("Email"); Response.Write("</td>");
+                Response.Write("<td>"); Response.Write("Number"); Response.Write("</td>");
+
+                Response.Write("</tr>");
+
+                foreach (DataRow dr in dt.Rows)
+                {
+                    Response.Write("<tr>");
+                    Response.Write("<td>"); Response.Write(dr["empName"].ToString()); Response.Write("</td>");
+                    Response.Write("<td>"); Response.Write(dr["empEmail"].ToString()); Response.Write("</td>");
+                    Response.Write("<td>"); Response.Write(dr["empContact"].ToString()); Response.Write("</td>");
+
+                    Response.Write("</tr>");
+                }
+                Response.Write("</table>");
+                con.Close();
+            }
+
+        }
+    }
+}
---
+++ AjaxExample2/SingleDetailSearch.aspx
@@ -0,0 +1,35 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="SingleDetailSearch.aspx.cs" Inherits="AjaxExample2.SingleDetailSearch" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+          <table>
+              <tr>
+                  <td>
+                      <asp:DropDownList ID="ddlName"  runat="server" AutoPostBack="false" DataSourceID="SqlDataSource1" DataTextField="empName" DataValueField="empId" OnChange="search()"></asp:DropDownList>
+                      <asp:SqlDataSource ID="SqlDataSource1" runat="server" ConnectionString="<%$ ConnectionStrings:DB_AjaxConnectionString %>" SelectCommand="SELECT * FROM [employee]"></asp:SqlDataSource>
+                  </td>
+              </tr>
+          </table>
+         <br />
+        <div id="d1">
+
+        </div>
+    </form>
+    <script type="text/javascript">
+        function search() {
+            var msg = new XMLHttpRequest();
+            msg.open("GET", "Insert.aspx?opr=search&name=" + document.getElementById("ddlName").value, false);
+            msg.send(null);
+
+            document.getElementById("d1").innerHTML = msg.responseText;
+            
+        }
+    </script>
+</body>
+</html>
